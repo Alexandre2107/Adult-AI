fc93052fc98ed5954d25d234cbed48579bfd8b49
Alexandre Rodrigues <85518165+Alexandre2107@users.noreply.github.com>
2024-07-31 20:32:18 -0300

    Implementation 14(Final)

--- Adult.R
+++ Adult.R
@@ -132,6 +132,8 @@ test_data <- read.csv(file = "adultTest.csv")
 colnames(train_data) <- c("age", "workclass", "fnlwgt", "education", "education_num", "marital_status", "occupation", "relationship", "race", "sex", "capital_gain", "capital_loss", "hours_per_week", "native_country", "income")
 
 colnames(test_data) <- c("age", "workclass", "fnlwgt", "education", "education_num", "marital_status", "occupation", "relationship", "race", "sex", "capital_gain", "capital_loss", "hours_per_week", "native_country", "income")
+test_data$income <- gsub(" >50K.", " >50K", test_data$income)
+test_data$income <- gsub(" <=50K.", " <=50K", test_data$income)
 
 # Identificando colunas numéricas
 numeric_columns <- sapply(train_data, is.numeric)
@@ -162,10 +164,16 @@ for (col in names(train_data)[numeric_columns]) {
 }
 
 # 8. Identificação e eliminação de atributos não necessários
-#é a copia de education
-database$education_num <- remove()
+#é a copia de education_num 
+train_data$education <- remove()
 #Não é um dado significante para a base de dados
-database$fnlwgt <- remove()
+train_data$fnlwgt <- remove()
+#Relationship é um subgrupo de marital_status
+train_data$relationship <- remove()
+
+test_data$education <- remove()
+test_data$fnlwgt <- remove()
+test_data$relationship <- remove()
 
 # 9. Identificação e eliminação de exemplos não necessários
 
@@ -227,23 +235,350 @@ prop.table(table(test_data_balanced$income))
 
 
 
+# 12. Limpeza de dados
+# Substituir " ?" por NA
+train_data[train_data == " ?"] <- NA
+test_data[test_data == " ?"] <- NA
+
+# Imprimir a porcentagem de valores NA em cada coluna para verificar
+print(sapply(train_data, function(col) {
+  sum(is.na(col)) * 100 / length(col)
+}))
+
+print(sapply(test_data, function(col) {
+  sum(is.na(col)) * 100 / length(col)
+}))
+
+# Função para calcular a moda
+Modes <- function(x) {
+  ux <- unique(x)
+  ux[which.max(tabulate(match(x, ux)))]
+}
+
+# Substituir NA pela moda em cada coluna do train_data
+train_data <- data.frame(lapply(train_data, function(col) {
+  if (any(is.na(col))) {
+    col[is.na(col)] <- Modes(col[!is.na(col)])
+  }
+  return(col)
+}))
+
+# Substituir NA pela moda em cada coluna do test_data
+test_data <- data.frame(lapply(test_data, function(col) {
+  if (any(is.na(col))) {
+    col[is.na(col)] <- Modes(col[!is.na(col)])
+  }
+  return(col)
+}))
+
+# Verificar duplicatas no train_data
+duplicated_train <- duplicated(train_data)
+any_duplicated_train <- any(duplicated_train)
+print(paste("Existem linhas duplicadas em train_data:", any_duplicated_train))
+
+# Verificar duplicatas no test_data
+duplicated_test <- duplicated(test_data)
+any_duplicated_test <- any(duplicated_test)
+print(paste("Existem linhas duplicadas em test_data:", any_duplicated_test))
 
 
+# Remover linhas duplicadas do train_data
+train_data <- train_data[!duplicated(train_data), ]
+
+# Remover linhas duplicadas do test_data
+test_data <- test_data[!duplicated(test_data), ]
+
+# Verificar novamente duplicatas no train_data
+duplicated_train <- duplicated(train_data)
+any_duplicated_train <- any(duplicated_train)
+print(paste("Existem linhas duplicadas em train_data:", any_duplicated_train))
+
+# Verificar novamente duplicatas no test_data
+duplicated_test <- duplicated(test_data)
+any_duplicated_test <- any(duplicated_test)
+print(paste("Existem linhas duplicadas em test_data:", any_duplicated_test))
 
 
-#database$Work.Class <- ifelse(database$Work.Class == " Private", 0, database$Work.Class)
-#database$Work.Class <- ifelse(database$Work.Class == " Self-emp-not-inc", 1, database$Work.Class)
-#database$Work.Class <- ifelse(database$Work.Class == " Self-emp-inc", 2, database$Work.Class)
-#database$Work.Class <- ifelse(database$Work.Class == " Federal-gov", 3, database$Work.Class)
-#database$Work.Class <- ifelse(database$Work.Class == " Local-gov", 4, database$Work.Class)
-#database$Work.Class <- ifelse(database$Work.Class == " State-gov" , 5, database$Work.Class)
-#database$Work.Class <- ifelse(database$Work.Class == " Never-worked", 6, database$Work.Class)
-#database$Work.Class <- ifelse(database$Work.Class == " Without-pay", 7, database$Work.Class)
-#database$Work.Class <- ifelse(database$Work.Class == " ?", 8, database$Work.Class)
+# Imprimir a porcentagem de valores NA em cada coluna para verificar
+print(sapply(train_data, function(col) {
+  sum(is.na(col)) * 100 / length(col)
+}))
 
-#database <- subset(database, Work.Class != " ?")
+print(sapply(test_data, function(col) {
+  sum(is.na(col)) * 100 / length(col)
+}))
 
-#database$Work.Class
+# Verificar a moda para colunas específicas
+print(Modes(train_data$workclass))
+print(Modes(train_data$education_num))
+print(Modes(train_data$occupation))
+print(Modes(train_data$hours_per_week))
+print(Modes(train_data$native_country))
 
-#boxplot(database$Capital.Gain, main= "boxplot capital_gain")
-#boxplot(database$Capital.loss, main= "boxplot capital_loss")
+print(Modes(test_data$workclass))
+print(Modes(test_data$occupation))
+print(Modes(test_data$native_country))
+
+table(train_data$workclass)
+table(test_data$workclass)
+
+#Criação de grupos para alocar grupos parecidos (WorkClass)
+train_data$workclass <- as.character(train_data$workclass)
+
+train_data$workclass[train_data$workclass == " Without-pay" | 
+                   train_data$workclass == " Never-worked"] <- " Unemployed"
+
+train_data$workclass[train_data$workclass == " State-gov" |
+                  train_data$workclass == " Local-gov"] <- " SL-gov"
+
+train_data$workclass[train_data$workclass == " Self-emp-inc" |
+                  train_data$workclass == " Self-emp-not-inc"] <- " Self-employed"
+
+table(train_data$workclass)
+
+test_data$workclass <- as.character(test_data$workclass)
+
+test_data$workclass[test_data$workclass == " Without-pay" | 
+                   test_data$workclass == " Never-worked"] <- " Unemployed"
+
+test_data$workclass[test_data$workclass == " State-gov" |
+                  test_data$workclass == " Local-gov"] <- " SL-gov"
+
+test_data$workclass[test_data$workclass == " Self-emp-inc" |
+                  test_data$workclass == " Self-emp-not-inc"] <- " Self-employed"
+
+table(test_data$workclass)
+
+#Criação de grupos para alocar grupos parecidos (Marital Status)
+table(train_data$marital_status)
+
+train_data$marital_status <- as.character(train_data$marital_status)
+
+train_data$marital_status[train_data$marital_status == " Married-AF-spouse" |
+                       train_data$marital_status == " Married-civ-spouse" |
+                       train_data$marital_status == " Married-spouse-absent"] <- " Married"
+
+train_data$marital_status[train_data$marital_status == " Divorced" |
+                       train_data$marital_status == " Separated" |
+                       train_data$marital_status == " Widowed"] <- " Not-Married"
+table(train_data$marital_status)
+
+table(test_data$marital_status)
+
+test_data$marital_status <- as.character(test_data$marital_status)
+
+test_data$marital_status[test_data$marital_status == " Married-AF-spouse" |
+                       test_data$marital_status == " Married-civ-spouse" |
+                       test_data$marital_status == " Married-spouse-absent"] <- " Married"
+
+test_data$marital_status[test_data$marital_status == " Divorced" |
+                       test_data$marital_status == " Separated" |
+                       test_data$marital_status == " Widowed"] <- " Not-Married"
+table(test_data$marital_status)
+
+#Criação de grupos para alocar grupos parecidos (Native Country)
+train_data$native_country <- as.character(train_data$native_country)
+
+north_america <- c(" Canada", " Cuba", " Dominican-Republic", " El-Salvador", " Guatemala",
+                   " Haiti", " Honduras", " Jamaica", " Mexico", " Nicaragua",
+                   " Outlying-US(Guam-USVI-etc)", " Puerto-Rico", " Trinadad&Tobago",
+                   " United-States")
+asia <- c(" Cambodia", " China", " Hong", " India", " Iran", " Japan", " Laos",
+          " Philippines", " Taiwan", " Thailand", " Vietnam")
+south_america <- c(" Columbia", " Ecuador", " Peru")
+europe <- c(" England", " France", " Germany", " Greece", " Holand-Netherlands",
+            " Hungary", " Ireland", " Italy", " Poland", " Portugal", " Scotland",
+            " Yugoslavia")
+other <- c(" South")
+train_data$native_country[train_data$native_country %in% north_america] <- " North America"
+train_data$native_country[train_data$native_country %in% asia] <- " Asia"
+train_data$native_country[train_data$native_country %in% south_america] <- " South America"
+train_data$native_country[train_data$native_country %in% europe] <- " Europe"
+train_data$native_country[train_data$native_country %in% other] <- " Other"
+
+table(train_data$native_country)
+
+test_data$native_country <- as.character(test_data$native_country)
+
+north_america <- c(" Canada", " Cuba", " Dominican-Republic", " El-Salvador", " Guatemala",
+                   " Haiti", " Honduras", " Jamaica", " Mexico", " Nicaragua",
+                   " Outlying-US(Guam-USVI-etc)", " Puerto-Rico", " Trinadad&Tobago",
+                   " United-States")
+asia <- c(" Cambodia", " China", " Hong", " India", " Iran", " Japan", " Laos",
+          " Philippines", " Taiwan", " Thailand", " Vietnam")
+south_america <- c(" Columbia", " Ecuador", " Peru")
+europe <- c(" England", " France", " Germany", " Greece", " Holand-Netherlands",
+            " Hungary", " Ireland", " Italy", " Poland", " Portugal", " Scotland",
+            " Yugoslavia")
+other <- c(" South")
+test_data$native_country[test_data$native_country %in% north_america] <- " North America"
+test_data$native_country[test_data$native_country %in% asia] <- " Asia"
+test_data$native_country[test_data$native_country %in% south_america] <- " South America"
+test_data$native_country[test_data$native_country %in% europe] <- " Europe"
+test_data$native_country[test_data$native_country %in% other] <- " Other"
+
+table(test_data$native_country)
+
+# 13. Identificação e conversão dos tipos de dados
+# Conversão dos valores de 'workclass' e 'marital_status'
+table(train_data$workclass)
+
+train_data$workclass <- ifelse(train_data$workclass == " Federal-gov", 0, train_data$workclass)
+train_data$workclass <- ifelse(train_data$workclass == " Private", 1, train_data$workclass)
+train_data$workclass <- ifelse(train_data$workclass == " Self-employed", 2, train_data$workclass)
+train_data$workclass <- ifelse(train_data$workclass == " SL-gov", 3, train_data$workclass)
+train_data$workclass <- ifelse(train_data$workclass == " Unemployed", 4, train_data$workclass)
+
+table(test_data$workclass)
+
+test_data$workclass <- ifelse(test_data$workclass == " Federal-gov", 0, test_data$workclass)
+test_data$workclass <- ifelse(test_data$workclass == " Private", 1, test_data$workclass)
+test_data$workclass <- ifelse(test_data$workclass == " Self-employed", 2, test_data$workclass)
+test_data$workclass <- ifelse(test_data$workclass == " SL-gov", 3, test_data$workclass)
+test_data$workclass <- ifelse(test_data$workclass == " Unemployed", 4, test_data$workclass)
+
+table(train_data$marital_status)
+
+train_data$marital_status <- ifelse(train_data$marital_status == " Married", 0, train_data$marital_status)
+train_data$marital_status <- ifelse(train_data$marital_status == " Not-Married", 1, train_data$marital_status)
+train_data$marital_status <- ifelse(train_data$marital_status == " Never-married", 2, train_data$marital_status)
+
+table(test_data$marital_status)
+
+test_data$marital_status <- ifelse(test_data$marital_status == " Married", 0, test_data$marital_status)
+test_data$marital_status <- ifelse(test_data$marital_status == " Not-Married", 1, test_data$marital_status)
+test_data$marital_status <- ifelse(test_data$marital_status == " Never-married", 2, test_data$marital_status)
+
+# Conversão dos valores 'occupation'
+table(train_data$occupation)
+
+train_data$occupation <- ifelse(train_data$occupation == " Adm-clerical", 0, train_data$occupation)
+train_data$occupation <- ifelse(train_data$occupation == " Armed-Forces", 1, train_data$occupation)
+train_data$occupation <- ifelse(train_data$occupation == " Craft-repair", 2, train_data$occupation)
+train_data$occupation <- ifelse(train_data$occupation == " Exec-managerial", 3, train_data$occupation)
+train_data$occupation <- ifelse(train_data$occupation == " Farming-fishing", 4, train_data$occupation)
+train_data$occupation <- ifelse(train_data$occupation == " Handlers-cleaners", 5, train_data$occupation)
+train_data$occupation <- ifelse(train_data$occupation == " Machine-op-inspct", 6, train_data$occupation)
+train_data$occupation <- ifelse(train_data$occupation == " Other-service", 7, train_data$occupation)
+train_data$occupation <- ifelse(train_data$occupation == " Priv-house-serv", 8, train_data$occupation)
+train_data$occupation <- ifelse(train_data$occupation == " Prof-specialty", 9, train_data$occupation)
+train_data$occupation <- ifelse(train_data$occupation == " Protective-serv", 10, train_data$occupation)
+train_data$occupation <- ifelse(train_data$occupation == " Sales", 11, train_data$occupation)
+train_data$occupation <- ifelse(train_data$occupation == " Tech-support", 12, train_data$occupation)
+train_data$occupation <- ifelse(train_data$occupation == " Transport-moving", 13, train_data$occupation)
+
+table(test_data$occupation)
+
+test_data$occupation <- ifelse(test_data$occupation == " Adm-clerical", 0, test_data$occupation)
+test_data$occupation <- ifelse(test_data$occupation == " Armed-Forces", 1, test_data$occupation)
+test_data$occupation <- ifelse(test_data$occupation == " Craft-repair", 2, test_data$occupation)
+test_data$occupation <- ifelse(test_data$occupation == " Exec-managerial", 3, test_data$occupation)
+test_data$occupation <- ifelse(test_data$occupation == " Farming-fishing", 4, test_data$occupation)
+test_data$occupation <- ifelse(test_data$occupation == " Handlers-cleaners", 5, test_data$occupation)
+test_data$occupation <- ifelse(test_data$occupation == " Machine-op-inspct", 6, test_data$occupation)
+test_data$occupation <- ifelse(test_data$occupation == " Other-service", 7, test_data$occupation)
+test_data$occupation <- ifelse(test_data$occupation == " Priv-house-serv", 8, test_data$occupation)
+test_data$occupation <- ifelse(test_data$occupation == " Prof-specialty", 9, test_data$occupation)
+test_data$occupation <- ifelse(test_data$occupation == " Protective-serv", 10, test_data$occupation)
+test_data$occupation <- ifelse(test_data$occupation == " Sales", 11, test_data$occupation)
+test_data$occupation <- ifelse(test_data$occupation == " Tech-support", 12, test_data$occupation)
+test_data$occupation <- ifelse(test_data$occupation == " Transport-moving", 13, test_data$occupation)
+
+# Conversão dos valores 'race'
+table(train_data$race)
+
+train_data$race <- ifelse(train_data$race == " Amer-Indian-Eskimo", 0, train_data$race)
+train_data$race <- ifelse(train_data$race == " Asian-Pac-Islander", 1, train_data$race)
+train_data$race <- ifelse(train_data$race == " Black", 2, train_data$race)
+train_data$race <- ifelse(train_data$race == " Other", 3, train_data$race)
+train_data$race <- ifelse(train_data$race == " White", 4, train_data$race)
+
+table(test_data$race)
+
+test_data$race <- ifelse(test_data$race == " Amer-Indian-Eskimo", 0, test_data$race)
+test_data$race <- ifelse(test_data$race == " Asian-Pac-Islander", 1, test_data$race)
+test_data$race <- ifelse(test_data$race == " Black", 2, test_data$race)
+test_data$race <- ifelse(test_data$race == " Other", 3, test_data$race)
+test_data$race <- ifelse(test_data$race == " White", 4, test_data$race)
+
+# Conversão dos valores 'sex'
+table(train_data$sex)
+
+train_data$sex <- ifelse(train_data$sex == " Male", 0, train_data$sex)
+train_data$sex <- ifelse(train_data$sex == " Female", 1, train_data$sex)
+
+table(test_data$sex)
+
+test_data$sex <- ifelse(test_data$sex == " Male", 0, train_data$sex)
+test_data$sex <- ifelse(test_data$sex == " Female", 1, train_data$sex)
+
+# Conversão dos valores 'native_country'
+
+table(train_data$native_country)
+
+train_data$native_country <- ifelse(train_data$native_country == " North America", 0, train_data$native_country)
+train_data$native_country <- ifelse(train_data$native_country == " Asia", 1, train_data$native_country)
+train_data$native_country <- ifelse(train_data$native_country == " South America", 2, train_data$native_country)
+train_data$native_country <- ifelse(train_data$native_country == " Europe", 3, train_data$native_country)
+train_data$native_country <- ifelse(train_data$native_country == " Other", 4, train_data$native_country)
+
+table(test_data$native_country)
+
+test_data$native_country <- ifelse(test_data$native_country == " North America", 0, test_data$native_country)
+test_data$native_country <- ifelse(test_data$native_country == " Asia", 1, test_data$native_country)
+test_data$native_country <- ifelse(test_data$native_country == " South America", 2, test_data$native_country)
+test_data$native_country <- ifelse(test_data$native_country == " Europe", 3, test_data$native_country)
+test_data$native_country <- ifelse(test_data$native_country == " Other", 4, test_data$native_country)
+
+# Conversão dos valores 'income'
+table(train_data$income)
+
+train_data$income <- ifelse(train_data$income == " <=50K", 0, train_data$income)
+train_data$income <- ifelse(train_data$income == " >50K", 1, train_data$income)
+
+table(test_data$income)
+
+test_data$income <- ifelse(test_data$income == " <=50K", 0, test_data$income)
+test_data$income <- ifelse(test_data$income == " >50K", 1, test_data$income)
+
+# 14. Análise e aplicação de alguma técnica para redução de dimensionalidade;
+
+head(train_data_balanced)
+
+# Normalização das variáveis numéricas
+numeric_cols <- sapply(train_data_balanced, is.numeric)
+train_data_num <- train_data_balanced[, numeric_cols]
+
+# Transformação de variáveis categóricas em variáveis dummy
+train_data_cat <- train_data_balanced[, !numeric_cols]
+
+# Usar a função model.matrix para criar variáveis dummy
+dummies <- model.matrix(~ . - 1, data = train_data_cat) # -1 para remover o intercepto
+
+# Combinar dados normalizados e variáveis dummy
+train_data_processed <- cbind(scale(train_data_num), dummies)
+
+# Aplicar PCA
+pca_result <- prcomp(train_data_processed, center = TRUE, scale. = TRUE)
+
+# Resumo dos resultados da PCA
+summary(pca_result)
+
+
+# Gráfico da variância explicada
+variance_explained <- summary(pca_result)$importance[2, ]
+barplot(variance_explained, main = "Proporção da Variância Explicada", 
+        xlab = "Componentes Principais", ylab = "Proporção da Variância")
+
+# Cálculo do número de componentes que explicam 95% da variância
+cumulative_variance <- cumsum(variance_explained)
+num_components <- which(cumulative_variance >= 0.95)[1]
+
+cat("Numero de componentes principais que explicam 95% da variancia:", num_components)
+
+
+# Obter as componentes principais
+pca_data <- as.data.frame(pca_result$x[, 1:num_components])
+
+# Visualizar os dados transformados
+head(pca_data)
